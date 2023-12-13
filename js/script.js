c7a92411c7ff75c53498698e0937bc3edad0b930
SebastianBuftea <sebibuftea85@gmail.com>
2023-12-13 16:37:26 +0100

    [UPDATE]add todolist

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const { createApp } = Vue
+
+createApp ({
+    data() {
+        return{
+            todolist:[
+                {
+                    text: "Buttare la spazzatura",
+                    done:false,
+                },
+                {
+                    text: "Pulire la stanza",
+                    done:false,
+                },
+                {
+                    text: "Fare i compiti",
+                    done:false,
+                },
+                {
+                    text: "Cucinare",
+                    done:false,
+                },
+                {
+                    text: "Portare il cane fuori ",
+                    done:false,
+                }
+            ]
+          
+        }
+    },
+    methods:{
+       
+    }
+}).mount("#app")
+
